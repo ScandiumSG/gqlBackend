6953bdbd0f01941e0be52f636a75ebc16e64404c
Stian Gaustad <stian.gaustad@no.experis.com>
2024-04-22 10:02:44 +0200

    Query - Added simple movie query

---
+++ gqlBackend.api/Queries/Query.cs
@@ -0,0 +1,10 @@
+﻿using gqlBackend.api.Models;
+
+namespace gqlBackend.api.Queries
+{
+    public class Query
+    {
+        public Movie GetMovie() => 
+            new Movie("Movie A", "Something happens", 2000, new List<Actor> { new Actor("Actor A", 42), new Actor("Actor B", 43) });
+    }
+}
