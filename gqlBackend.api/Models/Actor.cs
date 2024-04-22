06aab93eabd45509a07b848c2ffd5141d79d0b9b
Stian Gaustad <stian.gaustad@no.experis.com>
2024-04-22 10:02:31 +0200

    Models - Added some basic data models

---
+++ gqlBackend.api/Models/Actor.cs
@@ -0,0 +1,14 @@
+﻿namespace gqlBackend.api.Models
+{
+    public class Actor
+    {
+        public Actor(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ gqlBackend.api/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿namespace gqlBackend.api.Models
+{
+    public class Movie
+    {
+        public Movie(string name, string description, int year, ICollection<Actor> actors)
+        {
+            Name = name;
+            Description = description;
+            Year = year;
+            Actors = actors;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public ICollection<Actor> Actors { get; set; }
+    }
+}
